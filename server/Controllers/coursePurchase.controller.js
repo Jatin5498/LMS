4a50a50fc1a01e8a7164a16c9fba1f8f3f3bb004
Jatin5498 <132821813+Jatin5498@users.noreply.github.com>
2025-07-03 23:20:43 +0530

    Update coursePurchase.controller.js

--- server/Controllers/coursePurchase.controller.js
+++ server/Controllers/coursePurchase.controller.js
@@ -38,8 +38,8 @@ export const createCheckoutSession = async (req, res) => {
         },
       ],
       mode: "payment",
-      success_url: `http://localhost:5173/course-progress/${courseId}`, // once payment successful redirect to course progress page
-      cancel_url: `http://localhost:5173/course-detail/${courseId}`,
+      success_url: `https://learning-platform-rcby.onrender.com/course-progress/${courseId}`, // once payment successful redirect to course progress page
+      cancel_url: `https://learning-platform-rcby.onrender.com/course-detail/${courseId}`,
       metadata: {
         courseId: courseId,
         userId: userId,
